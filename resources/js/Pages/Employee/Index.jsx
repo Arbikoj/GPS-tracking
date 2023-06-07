5f029cc92bdd9bce00cac89336a91453a797797f
Arbikoj <kojosan15@gmail.com>
2023-06-08 01:21:33 +0700

    crd done kurang update masih error

--- resources/js/Pages/Employee/Index.jsx
+++ resources/js/Pages/Employee/Index.jsx
@@ -6,7 +6,10 @@ import React, { useEffect, useState } from "react"
 
 import axios from 'axios';
 import PrimaryButton from '@/Components/PrimaryButton';
-import Modal from '@/Components/Modal';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+// import { Button, Modal } from 'react-bootstrap';
 
 import Home from "./Index";
 
@@ -14,12 +17,23 @@ import Home from "./Index";
 
 const UsingFetch = ({auth}) => {
     // add data
+    const baseUrl = 'http://127.0.0.1:8000/';
+
+    const [dataku, setUsers] = useState([])
+    const [detail, setDetail] = useState([]);
     const [post, setPost ] = useState({
+        id:'',
         uid:'',
         name:'',
         gender:'',
         birth:''
     })
+
+    // modal
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     
     const handleInput = (event)=>{
@@ -33,38 +47,69 @@ const UsingFetch = ({auth}) => {
 
     function handleSubmit(event){
         event.preventDefault()
-        // axios.post('http://127.0.0.1:8000/api/employee', {post},{
-        //     headers: {'Content-Type': 'application/json'}
-        // })
-        // .then(response => console.log(response))
-        // .catch(err=> console.log(err))
-
-        // const userData = {
-        //     uid: post.uid,
-        //     name: post.password
-        //   };
-
-        axios.post("http://127.0.0.1:8000/api/employee", {
+        axios.post(baseUrl+"api/employee", {
         uid: post.uid,
         name: post.name,
         gender: post.gender,
         birth: post.birth
-      }, {
+    }, {
         headers: {'Content-Type': 'application/json'}
-      }).then(function(response) {
+    }).then(function(response) {
         {fetchData()}
         
         console.log(response);
-      }).catch(function(error) {
+    }).catch(function(error) {
         console.log(error);
+    })
+}
+// ,{
+//     headers: {'Content-Type': 'application/json'}
+// })
+const handleUpdate = updateId => {
+    axios.put(baseUrl+`api/employee/${updateId}`, { 
+        uid: post.uid,
+        name: post.name,
+        gender: post.gender,
+        birth: post.birth
+     }).then(response => {
+        console.log(response.data); // Optional: Handle the response
+        setPost(response.data);
+
+        {fetchData()}
+        // {fetchData()}
       })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+    const handleDelete = itemId => {
+        axios.delete(baseUrl+`api/employee/${itemId}`)
+          .then(response => {
+            {fetchData()}
+          })
+          .catch(error => {
+            console.error(error);
+          });
+      };
+
+
+
+
+      const handleDetail = itemId => {
+        axios.get(baseUrl+`api/employee/${itemId}`)
+        .then(response => {
+            console.log(response.data);
+            setPost(response.data);
+        })
+        .catch(error => {
+            console.error(error);
+          });
     }
 
-
-    const [dataku, setUsers] = useState([])
     var no = 1;
     const fetchData = () => {
-        fetch("http://127.0.0.1:8000/api/employee")
+        fetch(baseUrl+"api/employee")
         .then(response => {
             return response.json()
         })
@@ -77,9 +122,6 @@ const UsingFetch = ({auth}) => {
         fetchData();
     }, [])
 
-    const renderEmployees = () => {
-        
-    }
 
     return (
         <AuthenticatedLayout
@@ -91,52 +133,39 @@ const UsingFetch = ({auth}) => {
         <div className="py-12">
             <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div className="bg-white overflow-hidden p-5 shadow-sm sm:rounded-lg">
+                          <Button variant="primary" onClick={handleShow}>
+        Launch demo modal
+      </Button>
 
-                <form onSubmit={handleSubmit}>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+                <form >
                     uid : <input type="text" onChange={handleInput} value={post.uid} name='uid' />
                     name : <input type="text" onChange={handleInput} value={post.name} name='name' />
                     gender : <input type="text" onChange={handleInput} value={post.gender} name='gender' />
                     birth : <input type="text" onChange={handleInput} value={post.birth} name='birth' />
-                    <button>submit</button>
+                    <button onClick={handleSubmit}>submit</button>
+                    <button onClick={()=>handleUpdate(post.id)} className='p-2 bg-emerald-300'>edit</button>
+                    <button onSubmit={()=>handleUpdate(post.id)} className='p-2 bg-emerald-300'>editneww</button>
+
+                    {post.id}
+
                 </form>
-                {/* <form onSubmit={handleSubmit}>
-                <div className="mt-4">
-                    <InputLabel htmlFor="nama" value="text" />
-
-                    <TextInput
-                        type="text"
-                        name="uid"
-                        className="mt-1 block w-full"
-                        onChange={handleInput}
-                    />
-                        <TextInput
-                        type="text"
-                        name="name"
-                        className="mt-1 block w-full"
-                        onChange={handleInput}
-                    />
-                        <TextInput
-                        type="text"
-                        name="gender"
-                        className="mt-1 block w-full"
-                        onChange={handleInput}
-                    />
-                    <TextInput
-                        type="text"
-                        name="birth"
-                        className="mt-1 block w-full"
-                        onChange={handleInput}
-                    />
                 </div>
-                <PrimaryButton >
-                        Submit
-                    </PrimaryButton>
-                </form> */}
-
-
-
-                                </div>
-                            </div>
+            </div>
                     
                     </div>
 
@@ -147,6 +176,7 @@ const UsingFetch = ({auth}) => {
                         <th>Nama</th>
                         <th>Gender</th>
                         <th>Tanggal Lahir</th>
+                        <th>Aksi</th>
                     </tr>
                     {dataku.map((val, key) => {
                         return (
@@ -156,14 +186,57 @@ const UsingFetch = ({auth}) => {
                                 <td>{val.name}</td>
                                 <td>{val.gender}</td>
                                 <td>{val.birth}</td>
+                                <td>
+                                    <button onClick={()=>handleDelete(val.id)} className='p-2 bg-red-500'>delete</button>
+                                    <button onClick={()=>handleUpdate(val.id)} className='p-2 bg-emerald-300'>edit</button>
+                                    <button onClick={()=>handleDetail(val.id)} className='p-2 bg-red-500'>detail</button>
+
+
+                                </td>
+
                             </tr>
                         )
                         
                     })}
+
+                    
                 </table>
+
+                {detail ? (
+                        <div>
+                            <h1>{detail.id}</h1>
+                            <h1>{detail.uid}</h1>
+                            <h1>{detail.name}</h1>
+                            <h1>{detail.gender}</h1>
+                            <h1>{detail.birth}</h1>
+                            
+                        </div>
+                    ):(
+                        <p>data kosong</p>
+                    )}
     </AuthenticatedLayout>
 
     );
 }
 
 export default UsingFetch
+
+
+
+// hari ini
+
+// generate GKO
+
+// uid-datetime-lat-long
+
+// datetime
+// lat
+// long
+
+// datetime
+// lat
+// long
+
+// datetime
+// lat
+// long
--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -5,9 +5,11 @@ import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
+
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
 createInertiaApp({
+    
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
     setup({ el, App, props }) {
